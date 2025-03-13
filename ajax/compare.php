beaef8cd4cd8ac2081d035b83d260e6f9efca6cd
Henning <leutz@pcsg.de>
2025-03-13 10:41:33 +0100

    fix: refactor ajax files - typing
    Updated the version of `phpstan` in `.phive/phars.xml` to use 1.* and installed `1.12.13`.
    Refactored the `compare.php`, `preview.php`, `restore.php` files under `ajax/ directory`. Made the
    following key changes:
    - Made sure the function parameters are strongly typed.
    - Added condition to convert `date` parameter to int if it's string.
    - Updated function signatures to be multiline for better readability.
    - Added extra exceptions to `preview.php` function documentation.

--- ajax/compare.php
+++ ajax/compare.php
@@ -8,18 +8,30 @@ use QUI\Exception;
  *
  * @param String $project - Project data; JSON Array
  * @param Integer|String $id - Site-ID
- * @param                $date1
- * @param                $date2
+ * @param DateTime|int|string $date1
+ * @param DateTime|int|string $date2
  *
  * @return array
  * @throws Exception
  */
 
-function package_quiqqer_history_ajax_compare(string $project, int|string $id, $date1, $date2): array
-{
+function package_quiqqer_history_ajax_compare(
+    string $project,
+    int | string $id,
+    DateTime | int | string $date1,
+    DateTime | int | string $date2
+): array {
     $History = new QUI\History\Site();
     $Project = QUI::getProjectManager()->decode($project);
     $Site = $Project->get($id);
+
+    if (is_string($date1)) {
+        $date1 = (int)$date1;
+    }
+
+    if (is_string($date2)) {
+        $date2 = (int)$date2;
+    }
 
     $originalHTML = QUI\History\Site::getHTMLFromHistoryEntry($Site, $date1);
     $difference = $History->getDiffFromSite($Site, $date1, $date2);
--- ajax/restore.php
+++ ajax/restore.php
@@ -7,15 +7,22 @@ use QUI\History\Site;
  *
  * @param string $project - Project data; JSON Array
  * @param int|string $id - Site-ID
- * @param DateTime|int $date - Timestamp of the history entry
+ * @param DateTime|int|string $date - Timestamp of the history entry
  *
  * @throws \QUI\Exception
  */
-function package_quiqqer_history_ajax_restore(string $project, int|string $id, DateTime|int $date): void
-{
+function package_quiqqer_history_ajax_restore(
+    string $project,
+    int | string $id,
+    DateTime | int | string $date
+): void {
     $History = new Site();
     $Project = QUI::getProjectManager()->decode($project);
     $Site = $Project->get($id);
+
+    if (is_string($date)) {
+        $date = (int)$date;
+    }
 
     $History->restoreSite($Site, $date);
 }
